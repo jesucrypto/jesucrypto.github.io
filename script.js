29a7d90dfeeae1b3a51d27e68824f4b1502abe57
willy <aldarondo.42@gmail.com>
2019-11-20 16:49:41 -0400

    clasitronicos

--- script.js
+++ script.js
@@ -1,39 +1,27 @@
 $(document).ready(function(){
-  $(window).scroll(function(){
-    pos1 = $(window).scrollTop();
-    pos2 = $(".bohio-diagram").offset().top - 30;
-    console.log(pos2);
-    if (pos1 > pos2) {
-      $(".navbar").css({
-        backgroundColor : 'white',
-        boxShadow : '5px 0px 5px lightgrey'
-      });
+  $(".nav-item").hover(
+    function(){
+      $(this).children(".dropdown").css("display", "flex");
+    },
+    function(){
+      $(this).children(".dropdown").css("display", "none");
     }
-    if (pos2 > pos1) {
-      $(".navbar").css({
-        backgroundColor : 'rgba(0,0,0,0)',
-        boxShadow : 'none'
-      });
-    }
-  });
+  );
+
+  $(".toggle").click(function() {
+    let menu = $("#nav-items");
+    let show = menu.attr("class");
+    if (show === "") menu.attr("class", "nav-items");
+    else menu.attr("class", "");
+  })
 
   $(".nav-item").click(function(){
-    var text = $(this).text();
-    var elemClass;
-    var offset = 0;
-    if (text === "COMIENZO") elemClass = ".landing-area"
-    if (text === "FUNCIONES") elemClass = ".features-area"
-    if (text === "PRECIO") elemClass = ".pricing-area"
-    if (text === "INSCRIBETE") elemClass = ".movement-area"
-    var toElem = $(elemClass);
+    var text = $(this).children("span").text().trim();
+    var pos = $(".cat-title:contains("+text+")").offset().top;
     $("html, body").animate({
-      scrollTop : toElem.offset().top - offset
-    }, 200);
+      scrollTop: pos
+    });
+    $("#nav-items").attr("class", "");
   })
-
-  $(".call-to-action").click(function(){
-    $("html, body").animate({
-      scrollTop : $(".movement-area").offset().top
-    }, 200);
-  })
-})
+  
+});
