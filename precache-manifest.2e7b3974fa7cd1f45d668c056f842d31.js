aadeb65c9decc4d8d8e7a9821da4cb5790b355da
willy <aldarondo.42@gmail.com>
2019-12-02 11:45:51 -0400

    bohio

---
+++ precache-manifest.2e7b3974fa7cd1f45d668c056f842d31.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7494a01fc72f91c91da74cb147b8bda8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "554beaab7d8a17eac93d",
+    "url": "/static/css/main.9d4b75df.chunk.css"
+  },
+  {
+    "revision": "b8a796a31e51b568f568",
+    "url": "/static/js/2.71da95b3.chunk.js"
+  },
+  {
+    "revision": "554beaab7d8a17eac93d",
+    "url": "/static/js/main.e0514fbf.chunk.js"
+  },
+  {
+    "revision": "aaf3bc6507a0c8b96160",
+    "url": "/static/js/runtime-main.e6444462.js"
+  },
+  {
+    "revision": "1fce70541cd225507e29c233e23f2cb2",
+    "url": "/static/media/arrow.1fce7054.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/static/media/bars-solid.b3f49afd.svg"
+  },
+  {
+    "revision": "40790360813b8f99930b8526044df486",
+    "url": "/static/media/bohiologo.40790360.png"
+  },
+  {
+    "revision": "49dc1062ae3b0a623a08a8394398eefb",
+    "url": "/static/media/chinaGuineo.49dc1062.jpg"
+  }
+]);
